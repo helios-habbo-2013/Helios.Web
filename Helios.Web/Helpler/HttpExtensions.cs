16ea3073fb808acf24d67a56e1718022f246a5d2
Quackster <quacksterr.15@gmail.com>
2022-12-27 05:24:38 +1000

    Start templating

---
+++ Helios.Web/Helpler/HttpExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Helios.Web.Helpers
+{
+    public static class HttpExtensions
+    {
+        public static string AbsoluteContent(this IUrlHelper url, string contentPath)
+        {
+            HttpRequest request = url.ActionContext.HttpContext.Request;
+            return new Uri(new Uri(request.Scheme + "://" + request.Host.Value), url.Content(contentPath)).ToString();
+        }
+    }
+}
