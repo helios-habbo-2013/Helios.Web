aab5b910621376ce1463a2b0734de8a030985536
Quackster <quacksterr.15@gmail.com>
2023-01-16 00:16:10 +1000

    add random figure support

---
+++ Helios.Web/Util/Extensions/RandomExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace Helios.Web.Util.Extensions
+{
+    public static class RandomExtension
+    {
+        public static bool NextBoolean(this Random random)
+        {
+            return random.Next() > (Int32.MaxValue / 2);
+            // Next() returns an int in the range [0..Int32.MaxValue]
+        }
+    }
+}
---
+++ Helios.Web/Util/FigureUtil.cs
@@ -0,0 +1,114 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SixLabors.Fonts;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text;
+using System.Text.RegularExpressions;
+using Helios.Web.Util.Extensions;
+using Avatara.Figure;
+
+namespace Helios.Web.Util
+{
+    public class FigureUtil
+    {
+        #region Fields
+
+        public static readonly FigureUtil Instance = new FigureUtil();
+
+        #endregion
+
+        public FigureUtil()
+        {
+
+        }
+
+        public string GenerateRandomFigure(string genderReq)
+        {
+            StringBuilder figureOutput = new StringBuilder();
+
+            if (genderReq == null)
+            {
+                if (CaptchaUtil.Instance.Random.NextBoolean())
+                {
+                    genderReq = "M";
+                }
+                else
+                {
+                    genderReq = "F";
+                }
+            }
+
+            var randomTypes = new List<FigureSetType>();
+
+            foreach (var setType in FiguredataReader.Instance.FigureSetTypes.Values)
+            {
+                if (setType.IsMandatory != null && setType.IsMandatory.Value)
+                {
+                    if (!randomTypes.Any(x => x.Set == setType.Set))
+                        randomTypes.Add(setType);
+                }
+                else if ((setType.IsMaleMandatoryNonHC != null && setType.IsMaleMandatoryNonHC.Value && genderReq == "F") ||
+                    (setType.IsFemaleMandatoryNonHC != null && setType.IsFemaleMandatoryNonHC.Value && genderReq == "M"))
+                {
+                    if (!randomTypes.Any(x => x.Set == setType.Set))
+                        randomTypes.Add(setType);
+                }
+                else
+                {
+                    // not mandatory item but random chance we may add it :3
+
+                    if (setType.Set != "hr" && setType.Set != "sh")
+                        if (CaptchaUtil.Instance.Random.Next(0, 10) < 1)
+                            if (!randomTypes.Any(x => x.Set == setType.Set))
+                                randomTypes.Add(setType);
+
+                    if (setType.Set == "hr" || setType.Set == "sh")
+                        if (CaptchaUtil.Instance.Random.Next(0, 2) < 1)
+                            if (!randomTypes.Any(x => x.Set == setType.Set))
+                                randomTypes.Add(setType);
+                }
+            }
+
+
+            foreach (var setType in randomTypes)
+            {
+                var setTypeList = FiguredataReader.Instance.FigureSets.Values.Where(x => x.SetType == setType.Set && !x.Club && (x.Gender == genderReq || x.Gender == "U") && x.Selectable).ToList();
+
+                if (!setTypeList.Any())
+                {
+                    continue;
+                }
+
+                var randomlySelectedType = setTypeList[CaptchaUtil.Instance.Random.Next(0, setTypeList.Count)];
+
+                if (randomlySelectedType == null)
+                {
+                    continue;
+                }
+
+                var palettes = FiguredataReader.Instance.FigurePalettes[setType.PaletteId].Where(x => x.IsClubRequired == false).ToList();
+                var randomPaletteId = randomlySelectedType.Colourable ? int.Parse(palettes[CaptchaUtil.Instance.Random.Next(0, palettes.Count)].ColourId) : 1;
+
+                figureOutput.Append(randomlySelectedType.SetType);
+                figureOutput.Append("-");
+                figureOutput.Append(randomlySelectedType.Id);
+                figureOutput.Append("-");
+                figureOutput.Append(randomPaletteId);
+                figureOutput.Append(".");
+            }
+
+            var figure = figureOutput.ToString();
+            
+            if (figure.Length> 0)
+            {
+                figure = figure.Substring(0, figure.Length - 1);
+            }
+
+            return figure;
+        }
+    }
+}
